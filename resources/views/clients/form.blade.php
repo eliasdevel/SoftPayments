1801f618e35793fafc30e8fc9c455ebdaec35e4a
Adriano Almeida <adriano@oxigennio.com.br>
2015-05-22 09:16:24 -0300

    removido old do value do input

--- resources/views/clients/form.blade.php
+++ resources/views/clients/form.blade.php
@@ -15,32 +15,31 @@
     <!-- Name -->
     <div class="form-group">
         {!! Form::label('name', ucfirst(trans('form.name'))); !!}
-        {!! Form::text('name', old('name'), ['class' => 'form-control']); !!}
-        <!--{!! Form::text('name', old('name'), ['class' => 'form-control']); !!}-->
+        {!! Form::text('name', null, ['class' => 'form-control']); !!}
     </div>
 
     <!-- E-mail -->
     <div class="form-group">
         {!! Form::label('email', ucfirst(trans('form.email'))); !!}
-        {!! Form::email('email', old('email'), ['class' => 'form-control']); !!}
+        {!! Form::email('email', null, ['class' => 'form-control']); !!}
     </div>
 
     <!-- Company -->
     <div class="form-group">
         {!! Form::label('company', ucfirst(trans('form.company'))); !!}
-        {!! Form::text('company', old('company'), ['class' => 'form-control']); !!}
+        {!! Form::text('company', null, ['class' => 'form-control']); !!}
     </div>
 
     <!-- Phone -->
     <div class="form-group">
         {!! Form::label('phone', ucfirst(trans('form.phone'))); !!}
-        {!! Form::text('company', old('phone'), ['class' => 'form-control']); !!}
+        {!! Form::text('company', null, ['class' => 'form-control']); !!}
     </div>
 
     <!-- Cellphone -->
     <div class="form-group">
         {!! Form::label('cellphone', ucfirst(trans('form.cellphone'))); !!}
-        {!! Form::text('company', old('cellphone'), ['class' => 'form-control']); !!}
+        {!! Form::text('company', null, ['class' => 'form-control']); !!}
     </div>
 
     @include('layout.buttons', ['buttons' => ['submit']])
